3e8276e1d23f6bce81ab66adb282a678906abce5
MorganV <vandenberg.morgan@gmail.com>
2020-02-18 20:01:53 -0600

    temp code to remove .github files

--- CodepostFormatter.py
+++ CodepostFormatter.py
@@ -2,18 +2,36 @@ import pandas as pd
 import os
 import sys
 
-df = pd.read_csv('gitInfo.csv')
-df["GitHub"].str.lower()  # Convert usernames to lowercase
 
+projName = 'pa01-memmgr-'
+
+
+
+df = pd.read_csv('gitInfo.csv', index_col='ID')
+df['GitHub'] = df['GitHub'].str.lower()  # Convert usernames to lowercase
 
 print("Submisison root dir: " + sys.argv[1])
 projects = os.listdir(sys.argv[1])
-print("Renaming " + str(len(projects)) +
-      " directories to match email addresses.")
-projName = 'pa01-memmgr-'
+print("Renaming " + str(len(projects)) + " directories to match email addresses.")
+
+os.chdir(sys.argv[1])
+
 for proj in projects:
     # Get email from gitinfo
-    ghUsername = proj.replace(projName, "").trim().lower()
-    email = df[df.GitHub == ghUsername].Email
-    print("Rename " + proj + " to " + email)
-    #os.system("mv " + proj + " " + email)
+    os.chdir(proj)
+    os.system("rm -rf .github/")
+    os.system("rm CMakeCache.txt")
+    os.system("rm -rf CMakeFiles/")
+    os.system("rm Makefile")
+    
+    os.chdir("..")
+    continue
+    ghUsername = proj.strip().lower()
+    email = df[df.GitHub == ghUsername].Email.values
+    if len(email) == 0:
+          print(ghUsername + " didn't fill out the survey.")
+    else:
+          print("Rename " + proj + " to " + email[0])
+          command = 'mv ' + proj + ' ' + email
+          os.system(command[0])
+          
