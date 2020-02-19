b8815a8c4788a201fb8dae97b8d52f1b7cc4b668
MorganV <vandenberg.morgan@gmail.com>
2020-02-18 20:41:37 -0600

    add fixed format script for codepost

--- CodepostFormatter.py
+++ CodepostFormatter.py
@@ -6,32 +6,32 @@ import sys
 projName = 'pa01-memmgr-'
 
 
-
-df = pd.read_csv('gitInfo.csv', index_col='ID')
+df = pd.read_csv('gitInfo.csv', index_col='Timestamp')
 df['GitHub'] = df['GitHub'].str.lower()  # Convert usernames to lowercase
 
 print("Submisison root dir: " + sys.argv[1])
+print("Output root directory: " + sys.argv[2])
 projects = os.listdir(sys.argv[1])
-print("Renaming " + str(len(projects)) + " directories to match email addresses.")
+print("Renaming " + str(len(projects)) +
+      " directories to match email addresses.")
 
 os.chdir(sys.argv[1])
 
 for proj in projects:
     # Get email from gitinfo
-    os.chdir(proj)
-    os.system("rm -rf .github/")
-    os.system("rm CMakeCache.txt")
-    os.system("rm -rf CMakeFiles/")
-    os.system("rm Makefile")
-    
-    os.chdir("..")
-    continue
+
     ghUsername = proj.strip().lower()
     email = df[df.GitHub == ghUsername].Email.values
     if len(email) == 0:
-          print(ghUsername + " didn't fill out the survey.")
+        print(ghUsername + " didn't fill out the survey.")
     else:
-          print("Rename " + proj + " to " + email[0])
-          command = 'mv ' + proj + ' ' + email
-          os.system(command[0])
-          
+        os.chdir(sys.argv[1])
+        os.chdir(proj)
+        # Remove temporary file stuff
+        os.system("rm -rf CMakeFiles/")
+        os.system("rm -rf .github/")
+        os.system("rm -rf .git/")
+        os.chdir("..")
+
+        command = 'cp -r ' + proj + ' ' + sys.argv[2] + ' ' + email
+        os.system(command[0])
